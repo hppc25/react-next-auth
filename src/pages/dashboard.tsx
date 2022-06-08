057bf229a80eea2140a61cf764f2eda878ae2f60
Helder Correia <helder.patrick25@gmail.com>
2022-06-08 19:56:36 +0100

    validate permissions and role by Hook and Componente

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,18 +1,33 @@
 import { useContext, useEffect } from "react"
+
 import { AuthContext } from "../contexts/AuthContext"
 import { setupAPIClient } from "../services/api";
 import { api } from "../services/apiClient";
 import { withSSRAuth } from "../utils/withSSRAuth";
+import { useCan } from "../hooks/useCan";
+import { Can } from "../component/Can";
 
 export default function Dashboard() {
-  const { user } = useContext(AuthContext);
+    const { user } = useContext(AuthContext);
+
+//   const userCanSeeMetrics = useCan({
+//     roles: ['administrator'],
+//   })
+
 
   useEffect(() => {
     api.get('/me').then(response => console.log(response)).catch(err => console.log(err));
   }, []);
 
   return (
-    <h1>Dashboard: {user?.email}</h1>
+    <>
+      <h1>Dashboard: {user?.email}</h1>
+
+      {/* { userCanSeeMetrics && <div>Métricas</div>} */}
+      <Can permissions={['metrics.list']}>
+        <div>Métricas</div>
+      </Can>
+    </>
   )
 }
 
