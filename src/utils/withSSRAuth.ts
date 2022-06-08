e192c9733a7951e35e07d9d1ceb2de3eea954d33
Helder Correia <helder.patrick25@gmail.com>
2022-06-08 20:10:47 +0100

    Validate permissions by server side

--- src/utils/withSSRAuth.ts
+++ src/utils/withSSRAuth.ts
@@ -1,35 +1,63 @@
 import { GetServerSideProps, GetServerSidePropsContext, GetServerSidePropsResult } from "next";
 import { destroyCookie, parseCookies } from "nookies";
+import decode from 'jwt-decode';
 import { AuthTokenError } from "../services/errors/AuthTokenError";
-export function withSSRAuth<P>(fn: GetServerSideProps<P>) {
+import { validateUserPermissions } from "./validateUserPermissions";
+
+type WithSSRAuthOptions = {
+  permissions?: string[];
+  roles?: string[]; 
+}
+
+export function withSSRAuth<P>(fn: GetServerSideProps<P>, options?: WithSSRAuthOptions) {
   return async (context: GetServerSidePropsContext): Promise<GetServerSidePropsResult<P>> => {
     const cookies = parseCookies(context);
+    const token = cookies['@nextauth.token'];
 
-    if(!cookies['@nextauth.token']) {
+    if(!token) {
       return {
         redirect: {
           destination: '/',
           permanent: false,
         }        
       }
-    } 
+    }
 
-    
-    try {
-        return await fn(context)
-      } catch (err) {
-        console.log(err);
-        // if (err instanceof AuthTokenError) {
-          destroyCookie(context, '@nextauth.token')
-          destroyCookie(context, '@nextauth.refreshToken')
+    if (options) {
+      const user = decode<{permissions: string[], roles: string[]}>(token);
+      const { permissions, roles } = options;
   
-          return {
-            redirect: {
-              destination: '/',
-              permanent: false,
-            }
+      const userHasValidPermissions = validateUserPermissions({
+        user, 
+        permissions,
+        roles,
+      });
+
+      if(!userHasValidPermissions) {
+        return {
+          redirect: {
+            destination: '/dashboard',
+            permanent: false,
           }
-        // }
+        }
       }
+    }
+
+    try {
+      return await fn(context)
+    } catch (err) {
+      console.log(err);
+      // if (err instanceof AuthTokenError) {
+        destroyCookie(context, '@nextauth.token')
+        destroyCookie(context, '@nextauth.refreshToken')
+    
+        return {
+          redirect: {
+            destination: '/',
+            permanent: false,
+          }
+        }
+      // }
+    }
   }
 }
